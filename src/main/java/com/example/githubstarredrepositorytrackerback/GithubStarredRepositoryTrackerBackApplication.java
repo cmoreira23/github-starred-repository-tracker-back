c24fd4d77bbe94b78a37345cce7ca35710521931
Camilla Cyrino <camilla.m.uchoa@gmail.com>
2020-12-05 20:57:00 -0300

    Created spring boot project

---
+++ src/main/java/com/example/githubstarredrepositorytrackerback/GithubStarredRepositoryTrackerBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubstarredrepositorytrackerback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubStarredRepositoryTrackerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubStarredRepositoryTrackerBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/githubstarredrepositorytrackerback/GithubStarredRepositoryTrackerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubstarredrepositorytrackerback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubStarredRepositoryTrackerBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
